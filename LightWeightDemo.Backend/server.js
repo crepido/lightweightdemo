a889b67ec4b4a7255e36f5048a2a82af3a06953a
Glenn <glenn.lundberg@crepido.com>
2015-10-30 15:32:57 +0100

    Added inital boilerplate

---
+++ LightWeightDemo.Backend/server.js
@@ -0,0 +1,18 @@
+﻿var http = require('http');
+var port = process.env.port || 8080;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello World, I am the backend!\n');
+
+	// Retrieve
+	var MongoClient = require('mongodb').MongoClient;
+	
+	// Connect to the db
+	MongoClient.connect("mongodb://"+process.env.MONGO_PORT_27017_TCP_ADDR +":"+ process.env.MONGO_PORT_27017_TCP_PORT +"/exampleDb", function (err, db) {
+		if (!err) {
+			console.log("We are connected");
+		}
+	});
+}).listen(port);
+
+
